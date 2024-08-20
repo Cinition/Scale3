63e3c37ab63e07d2a14800fd1be5b1c90af1db6c
Cinition <cornee.n@gmail.com>
2024-08-20 05:42:16 +0200

    More dynamic camera

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -1,7 +1,14 @@
 extends Camera2D
 
-@export var Target: Node2D
+@export var Target:       Node2D
 @export var QubertOffset: Vector2
+@export var ZoomInSize:   Vector2
+@export var ZoomOutSize:  Vector2
+
+var DeltaZoom: Vector2
+
+func _ready() -> void:
+	DeltaZoom = self.zoom
 
 func _physics_process(delta: float) -> void:
 	if not Target:
@@ -9,3 +16,14 @@ func _physics_process(delta: float) -> void:
 
 	var NewPosition = lerp(position - QubertOffset, Target.position, 0.2)
 	position = Vector2(Target.position.x, NewPosition.y) + QubertOffset
+
+	var qubert = Target as Qubert
+	if not qubert:
+		return
+
+	var speedLerp = qubert.velocity.x / qubert.SlidingMovementSpeed
+	var cameraLerp = lerp(ZoomInSize, ZoomOutSize, speedLerp)
+	
+	DeltaZoom = lerp(DeltaZoom, cameraLerp, 0.01)
+	
+	self.zoom = DeltaZoom
